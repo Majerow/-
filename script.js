ee87f6b5ba9835dbac92ffbba9fd216f0b6ec2ab
Majerow <84037963+Majerow@users.noreply.github.com>
2021-05-11 20:41:05 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload=function(){
+    function showDate(){
+        var str = "";
+        var now = new Date();
+        str = now.toLocaleTimeString();
+        document.querySelector('.date').innerHTML = str;   
+    }
+    
+    setInterval(showDate, 1000);
+    $.getJSON("https://spreadsheets.google.com/feeds/list/1cwTPOXNloTpb2_lpm1qOXf2GZ-DyMiAMbkzPSgQlyb4/od6/public/values?alt=json",
+    function(data){
+        data = data['feed']['entry'];
+        console.log(data);
+        show(data);
+    }  
+    )
+    function show(data){
+        var out='';
+        for(var i = 0; i< data.length; i++){
+           out +=`<div class="card">`;
+           out +=`<h3 class="title">${data[i]['gsx$name']['$t']}</h3>`;
+           out +=`<img src="${data[i]['gsx$image']['$t']}" alt="">`;
+           out +=`<p class="price">Цена ${data[i]['gsx$price']['$t']}</p>`;
+           out +=`<button name="add_to_cart" data="${data[i]['gsx$article']['$t']}">Купити</button>`;
+           out +=`</div>`;
+        }
+        document.querySelector('.shop_field').innerHTML = out;
+    }
+    document.onclick = function(e){
+        console.log(e.target.attributes.name.nodeValue,e.target.attributes.data.nodeValue);
+    }
+}
